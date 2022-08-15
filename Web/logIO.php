0fb22c30b1061afda0d06f75f2a70be4d0dd0036
thanhnguyenxuan970 <thanhnguyenxuan970@gmail.com>
2022-08-15 09:30:27 +0700

    Thanh fixing

--- Web/logIO.php
+++ Web/logIO.php
@@ -1,36 +1,46 @@
 <?php
-  session_start();
+session_start();
 
-  if (isset($_POST['logout'])) {
+if (isset($_POST['logout'])) {
+  session_destroy();
+  header("Location: login.php");
+  exit();
+}
+
+if (isset($_POST['login'])) {
+  if (isset($_SESSION["user"])) {
     session_destroy();
-    header("Location: login.php");
-    exit();
   }
-
-  if (isset($_POST['login'])) {
-    if (isset($_SESSION["user"])) {
-      session_destroy();
-    }
-    if (isExist()) {
-      $_SESSION['user']['type'] = htmlentities($_POST["userType"], ENT_QUOTES);
-      header("Location: home.php");
-    } else {
-      header("Location: login.php");
-    }
+  if (isExist()) {
+    $_SESSION['user']['type'] = htmlentities($_POST["userType"], ENT_QUOTES);
+    header("Location: home.php");
   } else {
     header("Location: login.php");
   }
+} else {
+  header("Location: login.php");
+}
 ?>
 
 
 <?php
-function isExist() {
-  echo "1";
+function isExist()
+{
   include "config_mysql.php";
-  $username = $_POST['username'];
-  $password = $_POST['pass1'];
+  $username = $_POST["username"];
+  $password = $_POST["pass1"];
+
   $username_err = $password_err = $login_err = "";
-  echo "1";
+  echo "in";
+  // Check if username is empty
+  if (empty(trim($_POST["username"]))) {
+    $username_err = "Please enter username.";
+  }
+
+  // Check if password is empty
+  if (empty(trim($_POST["pass1"]))) {
+    $password_err = "Please enter your password.";
+  }
   // echo "User Type: ", $_POST['userType'], "<br>";
   // echo "Username: ", $_POST['username'], "<br>";
   // echo "Password: ", $_POST['pass1'], "<br>";
@@ -38,64 +48,28 @@ function isExist() {
   // Processing form data when form is submitted
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo "1";
-    // // Check if username is empty
-    // if (empty(trim($username))) {
-    //   $username_err = "Please enter username.";
-    // } else {
-    //   $username = trim($username);
-    // }
-
-    // // Check if password is empty
-    // if (empty(trim($password))) {
-    //   $password_err = "Please enter your password.";
-    // } else {
-    //   $password = trim($password);
-    // }
-
     // Validate credentials
     if (empty($username_err) && empty($password_err)) {
+      echo "2";
       // Prepare a select statement
       $sql = "SELECT * FROM users WHERE username = '$username'";
-
-      if ($stmt = mysqli_prepare($link, $sql)) {
-        // Bind variables to the prepared statement as parameters
-        mysqli_stmt_bind_param($stmt, "s", $param_username);
-
-        // Set parameters
-        $param_username = $username;
-
-        // Attempt to execute the prepared statement
-        if (mysqli_stmt_execute($stmt)) {
-          // Store result
-          mysqli_stmt_store_result($stmt);
-
-          // Check if username exists, if yes then verify password
-          if (mysqli_stmt_num_rows($stmt) == 1) {
-            // Bind result variables
-            mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
-            if (mysqli_stmt_fetch($stmt)) {
-              if (password_verify($password, $hashed_password)) {
-                // Password is correct, so start a new session
-                return TRUE;
-              } else {
-                // Password is not valid, display a generic error message
-                return FALSE;
-              }
-            }
+      echo "7";
+      $result = mysqli_query($connect, $sql);
+      if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_array($result)) {
+          if (password_verify($password, $row["password"])) {
+            return true;
           } else {
-            // Username doesn't exist, display a generic error message
-            return FALSE;
+            return false;
           }
-        } else {
-          return FALSE;
         }
-
-        // Close statement
-        mysqli_stmt_close($stmt);
+      } else {
+        echo '<script>alert("Wrong User Details")</script>';
+        return false;
       }
     }
-    
-    return FALSE; // True if the user exists in database; False otherwise.
+    // True if the user exists in database; False otherwise.
   }
-  
+  echo "8";
+  return FALSE;
 }
--- config_mysql.php
+++ config_mysql.php
@@ -3,7 +3,15 @@
 $server = "localhost";
 $port = "3306";
 $username = "root";
-$password = "ZedPaul1312@2001!#";
-$database = "db_course";
+$password = "123456";
+$database = "user_db";
 
-$pdo = new PDO("mysql:host=$server:$port;dbname=$database", $username, $password);
+try {
+    $conn = new PDO("mysql:host=$server;dbname=$database", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+  } catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+  }
+  ?>
